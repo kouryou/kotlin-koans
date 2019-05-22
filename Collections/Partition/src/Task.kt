c8295a7054af24f27445e556abd3fa5671c41f14
kouryou <23131663+kouryou@users.noreply.github.com>
2019-05-22 22:36:43 +0900

    Add Collections task9 answer

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,6 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> =
+        customers.filter {
+            val (deliveredOrders, undeliveredOrders) = it.orders.partition { it.isDelivered }
+            deliveredOrders.count() < undeliveredOrders.count()
+        }.toSet()
