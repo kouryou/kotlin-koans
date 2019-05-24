12a29ff2dbea9ae9537619796b716878d170de86
kouryou <23131663+kouryou@users.noreply.github.com>
2019-05-24 21:29:41 +0900

    Add Collections task10 answer

--- Collections/Fold/src/Task.kt
+++ Collections/Fold/src/Task.kt
@@ -1,4 +1,9 @@
 // Return the set of products that were ordered by every customer
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
+    val AllProducts = customers.flatMap { it.orders }.flatMap { it.products }
+    return customers.fold(AllProducts, {
+        products, customer ->
+        // filter only product which was ordered by each customer
+        products.filter { customer.orders.flatMap { it.products }.contains(it) }
+    }).toSet()
 }
